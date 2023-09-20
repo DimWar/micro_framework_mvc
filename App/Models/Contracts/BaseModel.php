94d01e2caa391e2289caa46d663780d7ffccf68d
Hala company <hadii.mohammadii1379@gmail.com>
2023-09-19 23:39:49 -0700

    complete basic basemodel class

--- App/Models/Contracts/BaseModel.php
+++ App/Models/Contracts/BaseModel.php
@@ -2,6 +2,19 @@
 
 namespace App\Models\Contracts ;
 
-abstract class BaseModel implements CrudInterface{
+abstract class BaseModel implements CrudInterface
+{
     protected $connection ;
+    protected $table ;
+    protected $primaryKey = 'id' ;
+    protected $pageSize = 10 ;
+    protected $attribute = [] ;
+
+    protected function __construct()
+    {
+        #if mysql => set connection mysql
+    }
+    protected function getAttribute(){
+        
+    }
 }
